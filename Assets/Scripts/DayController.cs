2c6c1fe6078d883a42e2ec01b429c95a97666794
Martin Håhjem Årdal <martaard@stud.ntnu.no>
2020-02-17 23:52:38 +0100

    62 - WIP Started implementing Observer Pattern
    Started implementing Observer Pattern so that the day controller can notify potential listeners (npc spawner) that it is night time and vice verca.

--- Assets/Scripts/DayController.cs
+++ Assets/Scripts/DayController.cs
@@ -3,18 +3,52 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+    DayController holds and changes what day cycle of day it is, either day time or night time.
+    After X seconds it changes the cycle of day and notifies listeners.
+    In this implementation, the length of night time and day time is equal.
+ */
 public class DayController : MonoBehaviour
 {
-    private const float DayLengthInSeconds = 600.0f;
+    private List<DayListener> dayListeners;
+    [SerializedField] private const float DayCycleLengthInSeconds = 600.0f;
+    [SerializedField] private DayCycle dayCycle;
 
     // Start is called before the first frame update
-    void Start()
-    {
-        InvokeRepeating("changeCycle", 0.0f, DayLengthInSeconds);
+    void Start() {
+        //InvokeRepating invokes a method first after 0 seconds, then every X seconds.
+        dayListeners = new List<DayListener>();
+        dayCycle = dayCycle.DayTime;
+        InvokeRepeating("changeCycle", 0.0f, DayCycleLengthInSeconds);
     }
 
+    //
     private void changeCycle() {
-            Debug.Log("SKIFTER DAG");
+        if (dayCycle == dayCycle.DayTime) {
+            dayCycle = dayCycle.NightTime;
+        }
+        else {
+            dayCycle = dayCycle.DayTime;
+        }
+        onCycleChange();
     }
 
+    // Tells listeners that the day is entering a new cycle,
+    // from daytime to nighttime, or from nighttime to daytime.
+    private void onCycleChange() {
+        foreach (DayListener dayListener in dayListeners)
+        {
+            dayListener.onChangeCycle(dayCycle);
+        }
+    }
+
+    //Adds day listeners.
+    public bool addListener(DayListner dayListner) {
+        if (dayListner == null) { 
+            return false;
+        }
+        dayListners.add(dayListner);
+        
+        return true;
+    }
 }
