2d7eddcb06d354c58b4c0b4ab03d1def51776e42
Martin Håhjem Årdal <martaard@stud.ntnu.no>
2020-02-17 15:48:50 +0100

    62 - Added dumb implementation of change of cycle
    InvokeRepeating is called every X seconds, which in turn calls method

---
+++ Assets/Scripts/DayController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayController : MonoBehaviour
+{
+    private const float DayLengthInSeconds = 600.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("changeCycle", 0.0f, DayLengthInSeconds);
+    }
+
+    private void changeCycle() {
+            Debug.Log("SKIFTER DAG");
+    }
+
+}
