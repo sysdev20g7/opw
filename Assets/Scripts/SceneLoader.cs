6bd34b176f11db04a340220800e477dc5431844c
Nils Jarle Haugen <nils+git@gaupne.net>
2020-03-03 16:35:06 +0100

    now working with transition

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -4,24 +4,25 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class SceneLoader : MonoBehaviour
-{
+public class SceneLoader : MonoBehaviour {
     public Animator animation;
 
-    public float animationDuration = 1f; 
+    public float animationDuration = 1f;
     // Update is called once per frame
 
-    public static int MAX_NUM_SCENES = 2; // Equals the number of valid scenes; see File -> Build settings in Unity...
+    public static int MAX_NUM_SCENES = 2; // Equals the number of valid scenes;
+                                          // see File -> Build settings in Unity...
+    private static bool DEBUG_SCENEMGMT = false;
     private int currentSceneIndex;
     private int requestedSceneIndex;
 
     void start() {
-        PrintDebug("Started SceneManager;index=" + currentSceneIndex);
-        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
-        PrintDebug("Updated to currentScene;index=" + currentSceneIndex);
+        if (DEBUG_SCENEMGMT) PrintDebug("Started SceneManager;index=" + currentSceneIndex);
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex; //Set initial index of current scene
+        if (DEBUG_SCENEMGMT) PrintDebug("Updated to currentScene;index=" + currentSceneIndex);
     }
+
     void Update() {
-        PrintDebug("Updated to currentScene;index=" + currentSceneIndex);
         if (Input.GetMouseButtonDown(0)) {
             PrintDebug("LeftClick");
             LoadNextScene(true);
@@ -32,45 +33,68 @@ public class SceneLoader : MonoBehaviour
         }
     }
 
-   /*
-    * The LoadNextScene loads the next or prevous scene, seen from the  index of the current scene.
-    * The build index order can be set under tile File -> Build Settings... menu
-    * @next true == loads the next scene / false == loads the prev scene
-    */
-    public void LoadNextScene( bool next) {
-        PrintDebug("LoadnextScene:" + next);
+    /*
+     * The LoadNextScene loads the next or prevous scene, seen from the  index of the current scene.
+     * The build index order can be set under tile File -> Build Settings... menu
+     * @next true == loads the next scene / false == loads the prev scene
+     */
+    public void LoadNextScene(bool next) {
+        PrintDebug("LoadNextScene:" + next);
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex; //Updates the scene index to current scene
         if (next) {
             requestedSceneIndex = currentSceneIndex + 1;
-            PrintDebug("Coroutine Start" + "CURR:" + currentSceneIndex + ";NEXT:" + requestedSceneIndex);
+            if (DEBUG_SCENEMGMT)
+                PrintDebug("Coroutine Start" + "CURR:" 
+                                             + currentSceneIndex + ";NEXT:"
+                                             + requestedSceneIndex);
             StartCoroutine(LoadScene(requestedSceneIndex));
-            PrintDebug("Coroutine End" + "CURR:" + currentSceneIndex);
-        } else {
+            if (DEBUG_SCENEMGMT) PrintDebug("Coroutine End" + "CURR:"
+                                                            + currentSceneIndex);
+        }
+        else {
             requestedSceneIndex = currentSceneIndex - 1;
-            PrintDebug("Coroutine Start" + "CURR:" + currentSceneIndex + ";NEXT:" + requestedSceneIndex);
+            if (DEBUG_SCENEMGMT)
+                PrintDebug("Coroutine Start" + "CURR:" + currentSceneIndex + ";NEXT:" + requestedSceneIndex);
             StartCoroutine(LoadScene(requestedSceneIndex));
-            PrintDebug("Coroutine End" + "CURR:" + currentSceneIndex);
+            if (DEBUG_SCENEMGMT) PrintDebug("Coroutine End" + "CURR:" + currentSceneIndex);
         }
     }
 
+    /*
+     *  This function can be used to print debug messages with a prefix to differentiate the
+     *  messages from other debug messages.
+     *  @param msg - The message to be printed
+     */
     private void PrintDebug(string msg) {
-        Debug.Log("DEBUG-SCENEMGT: CURR" + currentSceneIndex + "MSG:"+ msg);
-        
+        Debug.Log("DEBUG-SCENEMGT:" + msg);
     }
+
     /* This function loads a requested scene, as long as the scene requested has a valid
      index value. If the index value is not valid, the SceneManager will not load the scene.*/
     IEnumerator LoadScene(int sceneIndex) {
-        PrintDebug("ENUM-SCENEINDEX:" + sceneIndex);
+        if (DEBUG_SCENEMGMT) PrintDebug("ENUM-SCENEINDEX:" + sceneIndex);
         if (sceneIndex <= MAX_NUM_SCENES && sceneIndex > -1) {
+            Debug.Log("Switched from scene " + currentSceneIndex + " ("
+                      + SceneManager.GetSceneByBuildIndex(currentSceneIndex).name
+                      + ")");
+
             animation.SetTrigger("Begin");
             yield return new WaitForSeconds(1);
             SceneManager.LoadScene(sceneIndex);
-            PrintDebug("ENUM-LOADED:" + sceneIndex + "CURRENTSCENE IS:" + currentSceneIndex);
-        Debug.Log("ENUM-SWITCHED-TO"
-                  + currentSceneIndex + ": " + SceneManager.GetSceneByBuildIndex(currentSceneIndex).name);
-        } else {
-            Debug.Log("ENUM-INVALID-INDEX:\"" + currentSceneIndex + "\"");
+
+            Debug.Log("Switched to scene " + sceneIndex + " ("
+                      + SceneManager.GetSceneByBuildIndex(sceneIndex).name + ")");
+
+            if (DEBUG_SCENEMGMT)
+                PrintDebug("ENUM-LOADED:" + sceneIndex
+                                          + "CURRENTSCENE IS:"
+                                          + currentSceneIndex);
         }
-        currentSceneIndex = sceneIndex;
-        PrintDebug("ENUM-RETURN-CURRENTINDEX:" + currentSceneIndex);
+        else {
+            PrintDebug("ENUM-INVALID-INDEX: The scene \"" + sceneIndex
+                                                          + "\" requested is out of bounds. ");
+        }
+
+        if (DEBUG_SCENEMGMT) PrintDebug("ENUM-RETURN-CURRENTINDEX:" + currentSceneIndex);
     }
 }
