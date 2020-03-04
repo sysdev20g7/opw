ebfbbce9a54113c90266b5f77fa94c387ade32e6
Nils Jarle Haugen <nils+git@gaupne.net>
2020-03-04 22:35:25 +0100

    testing scene storing variables with  static scripts

---
+++ Assets/Scripts/GameData.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public static class GameData {
+    private static int _playerHealth;
+    private static int[] _currentPlayerItems;
+
+    /*
+     *  This function stores the player health to a static variable
+     * @param health - the health value
+     */
+    public static void SetPlayerHealth(int health) {
+        _playerHealth = health;
+    }
+    
+    /*
+     *  This function stores the player health to a static variable
+     * @param health - the health value
+     */
+    public static int GetPlayerHealth() {
+        return _playerHealth;
+    }
+    
+    /*
+     *  This function reads an static array with stored items and
+     *  returns them for use on the player
+     */
+    public static int[] GetCurrentPlayerItems() {
+        return _currentPlayerItems;
+    }
+    
+    /*
+     *  This function writes the current player items to a static array
+     *  @ param itemArray -- an array of items to store
+     */
+    public static void SetCurrentPlayerItems(int[] itemArray) {
+        _currentPlayerItems = itemArray;
+    }
+}
+
+
