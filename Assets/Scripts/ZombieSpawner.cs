dc482fce95257a6eea22dc3965bbe4c4fe6a4471
Erlend Solbakken Nikolaisen <erls@stud.ntnu.no>
2020-03-16 14:12:17 +0100

    Created simple spawn script
    Created a simple script for spawning zombies at set points.

---
+++ Assets/Scripts/ZombieSpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieSpawner : MonoBehaviour
+{
+    public GameObject enemy;
+    public int enemyCount;
+    public int maxEnemies;
+
+    private MoveSpots spawnPoints;
+    private int randomSpot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnPoints = FindObjectOfType<MoveSpots>();
+        StartCoroutine(EnemySpawn());
+    }
+
+    IEnumerator EnemySpawn() {
+        while (enemyCount < maxEnemies) {
+            randomSpot = Random.Range(0, spawnPoints.movespots.Length);
+            Instantiate(enemy, spawnPoints.movespots[randomSpot].position, Quaternion.identity);
+            yield return new WaitForSeconds(0.1f);
+            enemyCount += 1;
+        }
+    }
+}
