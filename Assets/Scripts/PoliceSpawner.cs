53b9614e368359283f208252c418b7002198da8e
Erlend Solbakken Nikolaisen <erls@stud.ntnu.no>
2020-03-17 14:57:25 +0100

    Added spawn script for police
    Added a script for spawning police and a spawner prefab with the script.

---
+++ Assets/Scripts/PoliceSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoliceSpawner : MonoBehaviour
+{
+    public GameObject enemy;
+    public GameObject spawner;
+    public int enemyCount;
+    public int maxEnemies;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(EnemySpawn());
+    }
+
+    IEnumerator EnemySpawn() {
+        //if (FindObjectsOfType<Police>().Length < maxEnemies) {
+            while (enemyCount < maxEnemies) {
+                Instantiate(enemy, spawner.transform.position, Quaternion.identity);
+                yield return new WaitForSeconds(0.4f);
+                enemyCount += 1;
+            }
+        //}
+    }
+}
