3f17f842a1411a4310e2bb203cef50c9d560064b
Erlend Solbakken Nikolaisen <erls@stud.ntnu.no>
2020-03-31 13:44:11 +0200

    Added velocity check to use correct animation
    Added a velocity check script to check which direction the enemy is walking in. This is then used to display the correct animation. Also Moved some scripts to another folder to organise.

---
+++ Assets/Scripts/Enemy/EnemyVelocityCheck.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyVelocityCheck : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    bool yNegative;
+    bool xNegative;
+    float velocityX;
+    float velocityY;
+    string direction;
+
+    public string FastestDirection() {
+        rb = GetComponent<Rigidbody2D>();
+        velocityX = rb.velocity.x;
+        velocityY = rb.velocity.y;
+        xNegative = velocityX < 0;
+        yNegative = velocityY < 0;
+
+        if (xNegative || yNegative) {
+            velocityY = System.Math.Abs(velocityY);
+            velocityX = System.Math.Abs(velocityX);
+            if ((velocityX > velocityY) && xNegative) {
+                direction = "Left";
+            }
+            else if (velocityY > velocityX) {
+                direction = "Right";
+            }
+            else {
+                direction = "Down";
+            }
+        }
+        else {
+            if (velocityX > velocityY) {
+                direction = "Right";
+            }
+            else {
+                direction = "Up";
+            }
+        }
+        return direction;
+    }
+}
--- Assets/Scripts/Enemy/FollowBehaviour.cs
+++ Assets/Scripts/Enemy/FollowBehaviour.cs
@@ -22,43 +22,83 @@ public class FollowBehaviour : StateMachineBehaviour
             animator.SetBool("isFollowing", false);
         }
 
-        //if (animator.GetComponent<Rigidbody2D>().velocity.x >= 0.01f) {
-        //    animator.SetBool("isRight", false);
-        //    animator.SetBool("isLeft", true);
-        //    animator.SetBool("isUp", false);
-        //    animator.SetBool("isDown", false);
-        //}
+        switch (animator.GetComponent<EnemyVelocityCheck>().FastestDirection()) {
+            case "Left":
+                animator.SetBool("isRight", false);
+                animator.SetBool("isLeft", true);
+                animator.SetBool("isUp", false);
+                animator.SetBool("isDown", false);
+                break;
 
-        //if (animator.GetComponent<Rigidbody2D>().velocity.x >= -0.01f) {
+            case "Right":
+                animator.SetBool("isRight", true);
+                animator.SetBool("isLeft", false);
+                animator.SetBool("isUp", false);
+                animator.SetBool("isDown", false);
+                break;
+
+            case "Up":
+                animator.SetBool("isRight", false);
+                animator.SetBool("isLeft", false);
+                animator.SetBool("isUp", true);
+                animator.SetBool("isDown", false);
+                break;
+
+            case "Down":
+                animator.SetBool("isRight", false);
+                animator.SetBool("isLeft", false);
+                animator.SetBool("isUp", false);
+                animator.SetBool("isDown", true);
+                break;
+
+            default:
+                break;
+        }
+
+        //if (animator.GetComponent<Rigidbody2D>().velocity.x >= 0.01f) {
         //    animator.SetBool("isRight", true);
         //    animator.SetBool("isLeft", false);
         //    animator.SetBool("isUp", false);
         //    animator.SetBool("isDown", false);
+        //    //animator.SetTrigger("Left");
         //}
 
-        //if (animator.GetComponent<Rigidbody2D>().velocity.y >= 0.01f) {
+        // if (animator.GetComponent<Rigidbody2D>().velocity.x <= -0.01f) {
+        //    animator.SetBool("isRight", false);
+        //    animator.SetBool("isLeft", true);
+        //    animator.SetBool("isUp", false);
+        //    animator.SetBool("isDown", false);
+        //    //animator.SetTrigger("Right");
+
+        //}
+
+        // if (animator.GetComponent<Rigidbody2D>().velocity.y >= 0.01f) {
         //    animator.SetBool("isRight", false);
         //    animator.SetBool("isLeft", false);
         //    animator.SetBool("isUp", true);
         //    animator.SetBool("isDown", false);
+        //    //animator.SetTrigger("Up");
         //}
 
-        //if (animator.GetComponent<Rigidbody2D>().velocity.y >= -0.01f) {
+        // if (animator.GetComponent<Rigidbody2D>().velocity.y <= -0.01f) {
         //    animator.SetBool("isRight", false);
         //    animator.SetBool("isLeft", false);
         //    animator.SetBool("isUp", false);
         //    animator.SetBool("isDown", true);
+        //    //animator.SetTrigger("Down");
         //}
     }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-
-    //}
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+        animator.SetBool("isRight", false);
+        animator.SetBool("isLeft", false);
+        animator.SetBool("isUp", false);
+        animator.SetBool("isDown", false);
+    }
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
-    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
     //    // Implement code that processes and affects root motion
     //}
 
