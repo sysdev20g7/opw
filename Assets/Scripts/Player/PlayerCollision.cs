783bf9218f593f16aac9598609dbf41b1d1c56be
Martin Håhjem Årdal <martaard@stud.ntnu.no>
2020-04-05 01:49:27 +0200

    111 - Added Player Collision.
    Added Player Collision through a PlayerCollision script on player. Uses OnCollisionEnter2D to detect the collision of a police object.
    This is however not an ideal implementation, as Player would need to know about ObjectController.

---
+++ Assets/Scripts/Player/PlayerCollision.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Defines the action on the player when a specific GameObject's collider
+/// enters the players Collision space.
+/// </summary>
+public class PlayerCollision : MonoBehaviour {
+
+    [SerializeField]
+    private ObjectController oc;
+
+    /// <summary>
+    /// Defines that when a police object's collision space enters the players collision
+    /// space, the player is caught by the cop and respawned.
+    /// </summary>
+    /// <param name="collision"></param>
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (oc != null) { 
+            if (collision.gameObject.tag == "Police") {
+                oc.playerCaughtByCop();
+            }
+        }
+    }
+}
