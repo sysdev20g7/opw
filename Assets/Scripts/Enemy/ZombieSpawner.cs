92fc6054f74493eaec7d574923b27bc22b000a7c
Martin Håhjem Årdal <martaard@stud.ntnu.no>
2020-04-14 19:37:04 +0200

    125 - Added internal DayCycle state to spawner, added despawn method.
    ZombieSpawner now acts as a listener to DayController, and will react to the different DayCycle state it is pushed.
    Added a Despawn Method that destroys the GameObjects of type Zombie. Will refactor to IEnumerator to be able to yield for some amount of time so that all zombies aren't instantly destroyed.

--- Assets/Scripts/Enemy/ZombieSpawner.cs
+++ Assets/Scripts/Enemy/ZombieSpawner.cs
@@ -1,8 +1,12 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class ZombieSpawner : MonoBehaviour
+/// <summary>
+/// ZombieSpawner spawns or despawns <i>n</i> zombies depending
+/// on if it is night or day. If it is night, it will spawn zombies, 
+/// if day, despawn.
+/// </summary>
+public class ZombieSpawner : MonoBehaviour, DayListener  
 {
     public GameObject enemy;
     public int enemyCount;
@@ -11,13 +15,60 @@ public class ZombieSpawner : MonoBehaviour
     private MoveSpots spawnPoints;
     private int randomSpot;
 
+    private DayCycle dayCycle;
+    private DayController dayController;
+
     // Start is called before the first frame update
+    // Acts as a initialzing method. 
     void Start()
     {
         spawnPoints = FindObjectOfType<MoveSpots>();
-        StartCoroutine(EnemySpawn());
+        var temp = GameObject.FindGameObjectWithTag("DayController");
+        if (temp != null) {
+            dayController = temp.GetComponent<DayController>();
+            dayController.addListener(this);
+            dayCycle = dayController.GetDayCycle();
+        }
     }
 
+    /// <summary>
+    /// Sets internal DayCycle state to retrieved value
+    /// and calls to change spawn state.
+    /// </summary>
+    /// <param name="dayCycle"></param>
+    public void onChangeCycle(DayCycle dayCycle) {
+        this.dayCycle = dayCycle;
+        changeSpawnState();
+        Debug.Log(this + "-Listener: Cycle changed to " + dayCycle);
+    }
+
+    /// <summary>
+    /// Decides based on what part of the day it is
+    /// wether Zombies should spawn or despawn.
+    /// </summary>
+    private void changeSpawnState() {
+        switch (dayCycle) {
+            case DayCycle.Dawn:
+                Debug.Log(this + " - Despawning Zombies");
+                DespawnEnemies();
+                break;
+            case DayCycle.DayTime:
+                //Do nothing.
+                break;
+            case DayCycle.Dusk:
+                //Do nothing
+                break;
+            case DayCycle.NightTime:
+                Debug.Log(this + " - Spawning Zombies");
+                StartCoroutine(EnemySpawn());
+                break;
+        }
+    }
+
+    /// <summary>
+    /// Spawns new Zombie GameObjects 
+    /// </summary>
+    /// <returns>new WaitForSeconds</returns>
     IEnumerator EnemySpawn() {
         int length = GameObject.FindGameObjectsWithTag("Zombie").Length;
         yield return new WaitForSeconds(1f);
@@ -28,6 +79,18 @@ public class ZombieSpawner : MonoBehaviour
                 Instantiate(enemy, spawnPoints.movespots[randomSpot].position, Quaternion.identity);
                 yield return new WaitForSeconds(0.4f);
                 enemyCount += 1;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Despawns zombie GameObjects if any zombies present in scene.
+    /// </summary>
+    private void DespawnEnemies() {
+        var zombieList = GameObject.FindGameObjectsWithTag("Zombie");
+        if (zombieList.Length > 0) { 
+            foreach (GameObject enemy in zombieList) {
+                Destroy(enemy);
             }
         }
     }
