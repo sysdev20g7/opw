f7b506981d57f5d701ae1324910cc6facd390549
Martin Håhjem Årdal <martaard@stud.ntnu.no>
2020-05-04 22:21:23 +0200

    57 - Reworked Melee Attack Script Structure

---
+++ Assets/Scripts/Attack/Attack.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Represents the attack of attack capable Game Object.
+/// The attack capable game object needs a AttackPoint game object as child
+/// to determine if a game object is in range. 
+/// If this other game object is an opposable game object,
+/// the attack capable game object can then attack this opposable game object.
+/// 
+/// The attack capable object needs an Animator and an AttackBehavior component
+/// to function properly. 
+/// </summary>
+public abstract class Attack : MonoBehaviour {
+    [SerializeField]
+    protected int attackDmg { get; set; }
+    //Default non-zero values for when max- and current health
+    //not set in Unity Inspector.
+    private readonly int defaultAttackDmg = 1;
+
+    protected bool IsAttacking { get; set; }
+    [SerializeField]
+    protected float attackTime;
+    [SerializeField]
+    protected float startTimeAttack;
+
+    protected Animator anim;
+
+}
---
+++ Assets/Scripts/Attack/PlayerAttack.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class PlayerAttack : Attack
+{
+    [SerializeField]
+    private Transform attackLocation;
+    [SerializeField]
+    private float attackRange;
+    [SerializeField] LayerMask enemies;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (attackTime <= startTimeAttack) {
+            if (Input.GetKeyDown(KeyCode.Space)) {
+                Debug.Log("ATTACKED");
+                anim.SetBool("IsAttacking", true);
+                Collider2D enemy =
+                    Physics2D.OverlapCircle(attackLocation.position, attackRange, enemies);
+                if (enemy != null) {
+                    enemy.gameObject.GetComponent<Health>().TakeDamage(attackDmg);
+                }
+            }
+        attackTime = startTimeAttack;
+        }
+        else {
+            attackTime -= Time.deltaTime;
+            anim.SetBool("IsAttacking", false);
+        }
+    }
+
+    private void OnDrawGizmosSelected() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackLocation.position, attackRange);
+    }
+}
