39a592cc6ebe6632fd1e5c83cd6afe6c2bfabd2f
Martin Håhjem Årdal <martaard@stud.ntnu.no>
2020-05-05 21:30:34 +0200

    57 - Add check so player can't move while attacking. This commit will...
    Add a check so that the player can't move while attacking, so that player doesn't go flying in a direction while attack animation is still playing.

--- Assets/Scripts/Player/PlayerMovementAlternative.cs
+++ Assets/Scripts/Player/PlayerMovementAlternative.cs
@@ -20,29 +20,31 @@ public class PlayerMovementAlternative : MonoBehaviour
     // Captures Player Input
     void Update() {
 
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
-        movement.Normalize();
+        if (!animator.GetBool("IsAttacking")) { 
+            movement.x = Input.GetAxisRaw("Horizontal");
+            movement.y = Input.GetAxisRaw("Vertical");
+            movement.Normalize();
 
-        animator.SetFloat("Horizontal", movement.x);
-        animator.SetFloat("Vertical", movement.y);
-        animator.SetFloat("Speed", movement.sqrMagnitude);
+            animator.SetFloat("Horizontal", movement.x);
+            animator.SetFloat("Vertical", movement.y);
+            animator.SetFloat("Speed", movement.sqrMagnitude);
 
-        if (movement.x == 1 ||
-            movement.x == -1 ||
-            movement.y == 1 ||
-            movement.y == -1) {
-            animator.SetFloat("LastHorizontal", movement.x);
-            animator.SetFloat("LastVertical", movement.y);
+            if (movement.x == 1 ||
+                movement.x == -1 ||
+                movement.y == 1 ||
+                movement.y == -1) {
+                animator.SetFloat("LastHorizontal", movement.x);
+                animator.SetFloat("LastVertical", movement.y);
+            }
         }
-
-        //if (Input.GetKeyDown(KeyCode.Space)) rb.velocity = Vector2.zero;
-
+        else {
+            rb.velocity = Vector2.zero;
+        }
     }
 
     //Updates the players position
     void FixedUpdate() {
-
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        if (!animator.GetBool("IsAttacking"))
+            rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
 }
