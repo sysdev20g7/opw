821f058d3b49e183948985eb2875b548556475f1
Martin Håhjem Årdal <martaard@stud.ntnu.no>
2020-05-06 13:45:07 +0200

    57 - Add new Enemy Melee Attack
    This commit will add new Enemy Melee Attack class so that Game Objects can attack Player.

---
+++ Assets/Scripts/Attack/EnemyMeleeAttack.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Represents the melee attack of an Enemy Game Object.
+/// An Enemy Game Object can only hurt the Player Game Object.
+/// The Enemy only hurts the player if the player collides with the enemy.
+/// </summary>
+public class EnemyMeleeAttack : MeleeAttack
+{
+    private Collision2D playerCollision;
+
+    void Start()
+    {
+        if (attackDmg == 0) attackDmg = defaultAttackDmg;
+    }
+
+    /// <summary>
+    /// Checks wether the colliding object is the player.
+    /// </summary>
+    /// <param name="collision"></param>
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.tag == "Player") {
+            playerCollision = collision;
+            Attack();
+        }
+    }
+
+    /// <summary>
+    /// Attacks the colliding player object.
+    /// </summary>
+    protected override void Attack()
+    {
+        Health playerHealth =
+            playerCollision.gameObject.GetComponent<Health>();
+        if (playerHealth != null) {
+            playerHealth.TakeDamage(attackDmg);
+        }
+    }
+}
